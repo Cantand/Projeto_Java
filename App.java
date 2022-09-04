ea9bc8c5df5de541ee04042dadcefe96ebd9075d
Charles Abreu <charlesabreu1127@gmail.com>
2022-09-04 07:23:25 -0300

    Estrutura-if

--- App.java
+++ App.java
@@ -37,25 +37,28 @@ public class App {
 
     /*Vamos trabalha valores na matemática */
 
-      int Contador = 0;
-      JOptionPane.showMessageDialog(null,Contador,"Matemática" , JOptionPane.QUESTION_MESSAGE);
-      Contador %= 10;
-      JOptionPane.showMessageDialog(null,Contador,"Matemática" , JOptionPane.QUESTION_MESSAGE);
-      Contador %= 10;
+    import javax.swing.JOptionPane;
 
-      Contador = 0;
-      JOptionPane.showMessageDialog(null,Contador,"Contador" , JOptionPane.QUESTION_MESSAGE);
-      Contador %= 10;
-      //Exemple somar na matemática 
-      int numeroA= 6;
-      int numeroB= 5;
-      int  resultado = (int)Math.sqrt(numeroA);
-      JOptionPane.showMessageDialog(null,resultado);
-
-      int numero;
-      Scanner leitor = new Scanner(System.in);
-      System.out.println("Introduce el numero de contador");
-      
-
+    public class App {
+       
+        public static void main(String[] args) throws Exception {
+         
+          Object[] op = {"Masculino", "Feminino"};
+          String resp = (String) JOptionPane.showInputDialog(null, "Selecione o sexo:\n",
+           "Pesquisa",
+            JOptionPane.PLAIN_MESSAGE, null, op, "Masculino");
+          if (resp == null) {
+            JOptionPane.showMessageDialog(null, "Você pressionou Cancelar");
+          }
+          if(resp == "Masculino") {
+            JOptionPane.showMessageDialog(null, "Você é homem.");
+          }
+    
+          if (resp == "Feminino") {
+            JOptionPane.showMessageDialog(null, "Você é mulher.");
+          }
+          System.exit(0);
+          }  
+    }
   }
 }
